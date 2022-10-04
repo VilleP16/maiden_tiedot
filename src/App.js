841d04141361ec62037d35f7ffb489507e1b4924
villep <pekvil16@gmail.com>
2022-10-04 10:11:54 +0300

    lisätty pääkaupungin säätiedot APIsta

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
+  
   const [countries, setCountries] = useState([])
   const [filterCountriesBy, setFilterCountriesBy] = useState('')
+  
 
   const handleFilterChange = (event) => {
     setFilterCountriesBy(event.target.value)
   }
 
   const showClickedCountry = value => () =>{
-    console.log('heiiiii', value)
+    //console.log('value',value)
     setFilterCountriesBy(value)
   }
 
   useEffect(() => {
-    console.log('effect')
+    //console.log('effect')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        console.log('promise fulfilled')
+        //console.log('promise fulfilled')
         //console.log(response.data)
         setCountries(response.data)
       })
   }, [])
-  console.log('render', countries.length, 'countries')
+  //console.log('render', countries.length, 'countries')
 
   const countriesToShow = countries.filter(function (country) {
 
@@ -49,7 +51,7 @@ const CountryList = ({ countries, handleClick }) => {
     return (
       <div>
         {countries.map((country) => {
-          console.log(country)
+          //console.log(country)
           return <CountryWithDetails
             name={country.name.common}
             capital={country.capital}
@@ -64,7 +66,7 @@ const CountryList = ({ countries, handleClick }) => {
     return (
       <div>
         {countries.map((country) => {
-          console.log(country)
+          //console.log(country)
           return <Country name={country.name.common} handleClick = {handleClick} key={country.name.official} />
         })}
       </div>
@@ -76,8 +78,7 @@ const Country = (props) => {
     <div>
       {props.name}
       <button onClick={props.handleClick(props.name)}>Show</button>
-    </div>
-     
+    </div> 
   )
 }
 
@@ -98,8 +99,44 @@ const CountryWithDetails = (props) => {
         })}
       </ul>
       <img src={props.flag} alt='Country flag' />
+      <Weather capital = {props.capital[0]}/>
     </div>
   )
+}
+
+const Weather = (props) =>{
+  const api_key = process.env.REACT_APP_API_KEY
+  const [capitalWeather, setCapitalWeather] = useState()
+  
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${props.capital}&appid=${api_key}`)
+      .then(response =>{
+        //console.log('response data',response.data)
+        setCapitalWeather(response.data)
+        //console.log('Capital weather state', capitalWeather)
+        
+        
+      })
+  }, [])
+
+  if(!capitalWeather){
+    console.log('palautti nullin')
+    return null
+  }else{
+    console.log('palautti tiedot')
+    return(
+      <div>
+        <h2>Weather in {props.capital}</h2>
+        <p>Temperature: {(capitalWeather.main.temp - 273.15).toFixed(2) } Celcius</p>
+        <p>Wind speed: {capitalWeather.wind.speed} m/s</p>
+        <p>Weather conditions: <b>{capitalWeather.weather[0].description}</b></p>
+        <img src={`http://openweathermap.org/img/wn/${capitalWeather.weather[0].icon}@2x.png`} alt="weather icon"/>
+      </div> 
+    )
+  }
+
+  
 }
 const FilterCountryList = (props) => {
   return (
